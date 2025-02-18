ab31e0e2fad7f993c1bd78f3cd7914497572868b
Vamshi Krishna Janagani <87365180+VamshiJanagani@users.noreply.github.com>
2025-02-19 00:00:14 +0530

    Create test_script.py
    Created Test script, now commiting this will have to trigger the .github workflow

---
+++ test_script.py
@@ -0,0 +1,10 @@
+import pytest
+
+def add(a, b):
+    return a + b
+
+def test_addition():
+    assert add(2, 3) == 5
+
+def test_failing_case():
+    assert add(2, 2) == 5  # ❌ This will fail to see how GitHub Actions handles failures
