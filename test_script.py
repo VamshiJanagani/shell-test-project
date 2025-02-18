ffa70f0f362089a802d2fb6f9dfc4b09a6def59e
Vamshi Krishna Janagani <87365180+VamshiJanagani@users.noreply.github.com>
2025-02-19 00:20:24 +0530

    Update test_script.py
    No failed test cases now

--- test_script.py
+++ test_script.py
@@ -7,4 +7,4 @@ def test_addition():
     assert add(2, 3) == 5  #This is a test comment 2
 
 def test_failing_case():
-    assert add(2, 2) == 5  # ❌ This will fail to see how GitHub Actions handles failures
+    assert add(2, 2) == 4  
